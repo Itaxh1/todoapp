c1f9881eeecb6275d45b2ccf722cebd959007b2e
Ashwin Kumar <meetashwin2000@gmail.com>
2022-04-04 12:35:56 +0530

    First Commit {Initalise Angular App}

---
+++ src/app/component/button/button.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input , Output, EventEmitter } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button'; 
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+  @Input() text: string;
+  @Input() color: string;
+  @Output() btnClick = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onClick(){
+    console.log("Add")
+  }
+
+}
